9f5cda6c08ec7ec113bc4c72d7bf25b2e5180246
magda sekulska <magdasekulska@wp.pl>
2018-03-02 21:05:50 +0100

    tictactoe

--- src/szost/Methods.java
+++ src/szost/Methods.java
@@ -14,21 +14,166 @@ public class Methods {
         System.out.println("\"kajak\"is palindrome =" + isPalindrom("kajak"));
         System.out.println("switched AbCd =" + switchCase("AbCd"));
         System.out.println("a;b;c; 0 (-1)=" + getNthIndexOf(';', "a;b;c", 0));
-        System.out.println("a;b;c; 1(1) =" +getNthIndexOf(';',"a;b;c", 1));
-        System.out.println("a;b;c; 2(3) =" +getNthIndexOf(';',"a;b;c", 2));
-        System.out.println("a;b;c; 3(5) =" +getNthIndexOf(';',"a;b;c", 3));
+        System.out.println("a;b;c; 1(1) =" + getNthIndexOf(';', "a;b;c", 1));
+        System.out.println("a;b;c; 2(3) =" + getNthIndexOf(';', "a;b;c", 2));
+        System.out.println("a;b;c; 3(5) =" + getNthIndexOf(';', "a;b;c", 3));
         System.out.println("\"Ala ma kota\" split =" + Arrays.toString(split("Ala ma kota", ' ')));
         System.out.println("\"ab;cd;ef\" split =" + Arrays.toString(split("ab;cd;ef", ';')));
         System.out.println("\"ab;cd;ef\" split =" + Arrays.toString(split("ab;;cd;ef", ';')));
         System.out.println("\"Ala ma kota\" split2 =" + Arrays.toString(split2("Ala ma kota", ' ')));
         System.out.println("\"ab;cd;ef\" split2 =" + Arrays.toString(split2("ab;cd;ef", ';')));
         System.out.println("\"ab;cd;ef\" split2 =" + Arrays.toString(split2("ab;;cd;ef", ';')));
+        System.out.println("fibonacci of 0 = " + fibbonaciNumber(0));
+        System.out.println("fibonacci of 1 = " + fibbonaciNumber(1));
+        System.out.println("fibonacci of 5 = " + fibbonaciNumber(5));
+        System.out.println("fibonacci of 10 = " + fibbonaciNumber(10));
+        System.out.println("fibonacci of 15 = " + fibbonaciNumber(15));
+        System.out.println("fibonacci of 25 = " + fibbonaciNumber(25));
+        System.out.println("fibonacci array (5) =" + Arrays.toString(fibonacciArray(5)));
+        System.out.println("fibonacci array (7) =" + Arrays.toString(fibonacciArray(7)));
+        System.out.println("fibonacci array (10) =" + Arrays.toString(fibonacciArray(10)));
+        System.out.println("reverse of \"abcdef\" = " + reverse("abcdef"));
+        System.out.println("reverse of \"abcdefg\" = " + reverse("abcdefg"));
+        System.out.println("\"(a+b\"is valid =" + isValidStatement("(a+b"));
+        System.out.println("reversed array {1,2,3,4,5,6} =" + Arrays.toString(reverseArray(new int[]{1, 2, 3, 4, 5, 6})));
+        System.out.println("tree of height 8 =\n" + tree(8));
+//        System.out.println("compare(\"123\", \"123\"(0) =" + compare("123","55"));
+        System.out.println("Primary numbers of 31 =" + primaryNumbersOf(31));
+        System.out.println("Primary numbers of 37 =" + primaryNumbersOf(37));
+        System.out.println("Primary numbers of 11 =" + primaryNumbersOf(11));
+
 
     }
 
-    // kod Fibbonaciego
-    public static int FibbonaciCode
+    public static String primaryNumbersOf(int number) {
+        int[] primaryNumbers = {1, 2, 3, 5, 7, 11};
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = primaryNumbers.length - 1; i >= 0; i--) {
+            while (number >= primaryNumbers[i]) {
+                number -= primaryNumbers[i];
+                stringBuilder.append(primaryNumbers[i]);
+                stringBuilder.append(" + ");
+            }
+        }
+        return stringBuilder.substring(0, stringBuilder.length() - 2);
+    }
 
+
+
+    //metoda do porównywania 2 stringów, ktore sa liczbami (metoda zwraca int, 1,-1,0)
+
+//    public static int compare{String first, String second) {
+//    int result = 0;
+//    if (first.length() > second.length()) {
+//        result = 1;
+//    }else if (first.length() < second.length()) {
+//        result = -1;
+//    } else {
+//        int i = 0;
+//        while (i < first.length()&& result == 0) {
+//            if (first.charAt(i) > second.charAt(i)){
+//                result = 1;
+//            } else if (first.charAt(i) < second.charAt(i)) {
+//                result = -1;
+//            }
+//            i++;
+//        }
+//    }
+//
+//    return result;
+//    }
+
+
+
+
+    //drzewko zwracajace stringa
+    public static String tree (int height) {
+        StringBuilder treeBuilder = new StringBuilder();
+        for (int i = 0; i < height ; i++) {
+            for (int j = 0; j < height - 1 - i; j++) {
+                treeBuilder.append (" ");
+            }
+            for (int j = 0; j < 2 * i +1; j++) {
+                treeBuilder.append("*");
+            }
+            treeBuilder.append ("\n");
+        }
+        return treeBuilder.toString();
+    }
+
+
+    //metoda zwracająca nowa tablice ktora jest odwrocona
+    public static int[] reverseArray (int[] array) {
+        int [] reversedArray = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            reversedArray[i] = array[array.length - i -1];
+        }
+        return reversedArray;
+    }
+
+
+    //sprawdzanie nawiasow w stringu ")("
+
+
+    public static boolean isValidStatement (String statement) {
+        char[] array = statement.toCharArray();
+        int counter = 0;
+        int i = 0;
+        while (i < array.length && counter >= 0) {
+            if (array[i] == '(') {
+                counter++;
+            } else if (array[i] == ')') {
+                counter++;
+            }
+            i++;
+        }
+        return counter == 0;
+    }
+
+    //odwracanie stringa test -> tset, abcde -> edcba
+
+    public static void swap () {
+        int a = 2;
+        int b = 5;
+        int tmp = a;
+        a = b;
+        b = tmp;
+    }
+
+    public static String reverse (String text) {
+        char [] array = text.toCharArray();
+        for (int i = 0; i < array.length / 2; i++) {
+            char tmp = array[i];
+            array[i] = array[array.length - 1 -i];
+            array[array.length - 1 -i] = tmp;
+        }
+        return String.valueOf(array);
+    }
+
+
+    public static int[] fibonacciArray (int toIndex) {
+    int [] array = new int[toIndex];
+    array [0] = 0;
+    array [1] = 1;
+        for (int i = 2; i < array.length ; i++) {
+            array[i] = array[i -1] + array[i-2];
+        }
+        return array;
+    }
+
+    // kod Fibbonaciego
+    public static int fibbonaciNumber (int n) {
+        if (n <= 1) return n;
+        int first = 0;
+        int second = 1;
+        for (int i = 2; i <= n; i++) {
+            int next = first + second;
+            first = second;
+            second = next;
+
+        }
+        return second;
+    }
 
     public static String [] split2 (String message, char c) {
         int size = Warmup.letterCounter(message, c) + 1;
@@ -178,9 +323,6 @@ public class Methods {
         //split -> 123; abc;dd ->{"123","abc","dd"]
         //         dd.aa.cc -> ["dd", "aa","cc"]
 
-        //fibonacci
-    //odwracanie stringa test -> tset, abcde -> edcba
-    //sprawdzanie nawiasow w stringu ")("
     // suma cyfr w stringu (wywloywanie jakich metod?) -> "1234"
         //sprawdzenie czy string jest liczba
         //sprwdzenie nawiasow w liczbie
---
+++ src/tictactoe/TicTacToeViews.java
@@ -0,0 +1,34 @@
+package tictactoe;
+
+import java.util.Scanner;
+
+public class TicTacToeViews {
+
+    public static void main(String[] args) {
+        char[] board = "123456789".toCharArray();
+        boardView(board);
+        board[4] = 'X';
+        boardView(board);
+    }
+
+    public static void menuView() {
+        System.out.println("1. start");
+        System.out.println("2. wyniki");
+    }
+
+    public static void boardView(char[] gameStatus) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < gameStatus.length; i++) {
+            stringBuilder.append(gameStatus[i]);
+            if (i != gameStatus.length - 1 && (i + 1) % 3 == 0) {
+                stringBuilder.append("\n");
+                stringBuilder.append("=====");
+                stringBuilder.append("\n");
+            } else if (i != gameStatus.length - 1) {
+                stringBuilder.append("|");
+            }
+            System.out.println(stringBuilder.toString());
+        }
+
+    }
+}
